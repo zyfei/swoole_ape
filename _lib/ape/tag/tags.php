88e6707eadcc9be80c3156afc5bec1bbfc25a398
张宇飞 <943901167@qq.com>
2019-01-18 10:19:40 +0800

    Revert "Initial commit"
    This reverts commit 2c9ca4ad425a43952b7291e38174a148097b0724.

--- _lib/ape/tag/tags.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 配置类标签注释
- */
-return array(
-	"httpController" => '\ape\tag\HttpControllerTag',
-	"middleware" => '\ape\tag\MiddlewareTag',
-	"view" => '\ape\tag\ViewTag'
-);
--- model/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace model;
-
-use ape\db\MysqlPool;
-
-class User extends MysqlPool {
-	public static $table = "t_user";
-	public static $softDelete = true;
-}
